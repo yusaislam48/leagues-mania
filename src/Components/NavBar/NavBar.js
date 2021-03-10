49e3087626a4d9156f9808aa46d37b85252e8c40
Yusa Islam <yusaislam48@gmail.com>
2021-03-11 01:00:35 +0600

    edit readme.md file

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -13,16 +13,13 @@ const NavBar = () => {
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/home">Home</a>
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="/">Features</a>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="/">Pricing</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" href="/" tabindex="-1" aria-disabled="true">Disabled</a>
         </li>
       </ul>
     </div>
